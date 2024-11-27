020a3507cc87192964702e9621a6366d005a2265
Aslam <aslam>
2024-11-27 18:10:26 +0530

    Update

--- routes/api.php
+++ routes/api.php
@@ -8,15 +8,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
     Route::post('/login', [UserController::class, 'login'])->name('login');
     Route::post('/register', [UserController::class, 'register'])->name('register');
-    Route::get('/quizzes', [QuizController::class, 'getQuizzes'])->name('getQuizzes');
-    Route::get('/quiz/{id}', [QuizController::class, 'getQuiz'])->name('getQuiz');
-    Route::get('/test', [QuizController::class, 'test'])->name('test');
 
     Route::get('/email/verify/{id}/{hash}', [UserController::class, 'emailVerify'])->middleware('signed')->name('verification.verify');
     Route::post('/email/resend', [UserController::class, 'emailResend'])->middleware('throttle:6,1');
 
     Route::post('/password/forgot', [UserController::class, 'passwordResetLink'])->name('password.forgot');
     Route::post('/password/reset', [UserController::class, 'resetPassword'])->name('password.reset');
+
+    Route::get('/quizzes', [QuizController::class, 'getQuizzes'])->name('getQuizzes');
+    Route::get('/quiz/{id}', [QuizController::class, 'getQuiz'])->name('getQuiz');
 });
 
 // Protected routes
@@ -25,4 +25,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/quiz/create', [QuizController::class, 'create'])->name('quiz.create');
     Route::get('/quiz/edit/{id}', [QuizController::class, 'edit'])->name('getQuiz');
     Route::post('/quiz/update/{id}', [QuizController::class, 'update'])->name('quiz.update');
+    Route::post('/quiz/generate', [QuizController::class, 'textToQuiz'])->name('quiz.generate');
 });
